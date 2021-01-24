1896a75974783be03b4da20ca65c29cfd9597ef5
Nathan Blair <nateblairfilm@gmail.com>
2021-01-24 16:05:22 -0600

    Switch to babylonJS game engine framework

--- js/script.js
+++ js/script.js
@@ -1,72 +1,75 @@
-class Camera
-{
-    constructor() {
-        this.x;
-        this.y;
-        this.width;
-        this.height;
-    }
-}
+import * as BABYLON from 'babylonjs';
 
 class NateHub
 {
     constructor() {
-        this.resizeTimer;
-        this.scaleRatio = 3;
-
-        this.world = {
-            width  : 4000,
-            height : 4000
-        }
-
-        this.camera = new Camera();
+        this.canvas;
+        this.engine;
 
         this.init();
     }
 
     init() {
         window.addEventListener('DOMContentLoaded', (event) => {
-            this.setScaleRatio();
-            this.create();
+            this.canvas = document.getElementById( 'nhCanvas' );
+            this.engine = new BABYLON.Engine( this.canvas, true );
+
+            this.createScene();
+            this.run();
         });
     }
 
-    setScaleRatio() {
-        this.scaleRatio = window.devicePixelRatio / 3;
-    }
-
-    positionX( percent ) {
-        return Math.round( percent * this.world.width );
-    }
-
-    positionY( percent ) {
-        return Math.round( percent * this.world.height );
-    }
-
-    placeLandmark( id, x, y, w, h ) {
-        const el = document.getElementById( id );
-        el.style.left = x;
-        el.style.top = y;
-
-        if ( w ) {
-            el.style.width = w;
-        }
-
-        if ( h ) {
-            el.style.width = w;
-        }
-    }
-
-    create() {
+    createScene() {
         console.group( 'NateHub' );
         console.log( this );
         console.groupEnd();
 
-        const body = document.getElementsByTagName('body')[0];
-        body.style.width = this.world.width + 'px';
-        body.style.height = this.world.height + 'px';
+        this.scene = new BABYLON.Scene( this.engine );
+        this.scene.clearColor = new BABYLON.Color3.White();
 
-        this.placeLandmark( 'home', this.positionX(.5), this.positionY(.5), '30px', '30px' );
+        this.home = BABYLON.Mesh.CreateSphere( "Home", 32, 4, this.scene );
+
+        this.train = BABYLON.Mesh.CreateBox( "Train", 1.5, this.scene );
+        this.train.position =  new BABYLON.Vector3( 0, 0, -8 );
+
+
+        this.base1 = BABYLON.Mesh.CreateSphere( "Code", 32, 4, this.scene );
+        this.base1.position = new BABYLON.Vector3( 200, 0, 200 );
+
+        this.base2 = BABYLON.Mesh.CreateSphere( "Art", 32, 4, this.scene );
+        this.base2.position = new BABYLON.Vector3( 200, 0, -200 );
+
+        this.base3 = BABYLON.Mesh.CreateSphere( "Career", 32, 4, this.scene );
+        this.base3.position = new BABYLON.Vector3( -200, 0, -200 );
+
+        this.base4 = BABYLON.Mesh.CreateSphere( "Contact", 32, 4, this.scene );
+        this.base4.position = new BABYLON.Vector3( -200, 0, 200 );
+ 
+
+        this.cameraDev = new BABYLON.FreeCamera(
+            'camera1',
+            new BABYLON.Vector3( 50, 500, -400 ),
+            this.scene
+        );
+        this.cameraDev.setTarget( BABYLON.Vector3.Zero() );
+        
+        // this.camera = new BABYLON.FollowCamera(
+        //     'followCam',
+        //     new BABYLON.Vector3( 0, 0, 0 ),
+        //     this.scene,
+        //     this.train
+        // );
+        // this.camera.heightOffset = 50;
+        // this.camera.radius = 80;
+        // this.camera.rotationOffset = 50;
+        // this.camera.attachControl( this.canvas, true );
+
+    }
+
+    run() {
+        this.engine.runRenderLoop( () => {
+            this.scene.render();
+        } );
     }
 
 }
