f9d17bc9201851a5e29e1ad4c9a0172951603657
Nathan Blair <nateblairfilm@gmail.com>
2022-07-10 20:21:50 -0500

    Start from scratch one more time

--- src/script.js
+++ src/script.js
@@ -1,32 +1,19 @@
 import './style.css';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
-import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer';
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
-import { GammaCorrectionShader } from 'three/examples/jsm/shaders/GammaCorrectionShader';
-import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-// import * as dat from 'lil-gui';
-import * as TWEEN from '@tweenjs/tween.js';
+// import * as TWEEN from '@tweenjs/tween.js';
 // import GUI from 'lil-gui';
 // import Hammer from 'hammerjs';
-
-import Camera from './camera';
 
 class NateHub {
   constructor() {
     this.canvas = document.querySelector('canvas.webgl');
-    this.lastDeltaY = [0];
-    this.points = [];
-    this.hovered = null;
-    this.clicked = null;
     this.interactiveElements = [];
-    this.focusableObjects = [];
 
     this.mouse = new THREE.Vector2();
     this.mouseRaycaster = new THREE.Raycaster();
-    this.clock = new THREE.Clock();
 
     // this.gui = new GUI();
 
@@ -41,14 +28,9 @@ class NateHub {
       height: window.innerHeight,
     };
 
-    this.materials = {
-      flatWhite: new THREE.MeshStandardMaterial({
-        color: new THREE.Color('#CCCCCC'),
-        roughness: 0.4,
-      }),
-    };
-
     this.setupScene();
+    this.light();
+    this.cube();
     this.bindEvents();
     this.tick();
   }
@@ -68,26 +50,25 @@ class NateHub {
   getScreenAspectRatio() {
     const windowWidth = this.sizes.width;
     const windowHeight = this.sizes.height;
-    return windowHeight / windowWidth;
+    return windowWidth / windowHeight;
   }
 
   bindEvents() {
-    window.addEventListener('resize', () => {
-      // Update sizes
-      this.sizes.width = window.innerWidth;
-      this.sizes.height = window.innerHeight;
+    window.addEventListener('resize', this.handleResize.bind(this));
+  }
 
-      // Update camera
-      this.camera.aspect = this.sizes.width / this.sizes.height;
-      this.camera.updateProjectionMatrix();
-      this.cameraController.setPosition();
+  handleResize() {
+    // Update sizes
+    this.sizes.width = window.innerWidth;
+    this.sizes.height = window.innerHeight;
 
-      // Update renderer
-      this.renderer.setSize(this.sizes.width, this.sizes.height);
-      this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    // Update camera
+    this.camera.aspect = this.getScreenAspectRatio();
+    this.camera.updateProjectionMatrix();
 
-      this.composer.setSize(this.sizes.width, this.sizes.height);
-    });
+    // Update renderer
+    this.renderer.setSize(this.sizes.width, this.sizes.height);
+    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
   }
 
   setupScene() {
@@ -95,7 +76,6 @@ class NateHub {
     this.textureLoader = new THREE.TextureLoader();
     this.loadingManager = new THREE.LoadingManager();
     this.gltfLoader = new GLTFLoader();
-    this.fontLoader = new FontLoader();
 
     this.renderer = new THREE.WebGLRenderer({
       canvas: this.canvas,
@@ -105,53 +85,32 @@ class NateHub {
     this.renderer.setSize(this.sizes.width, this.sizes.height);
     this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-    this.cube = this.gltfLoader.load(
-      'natecube.gltf',
-      (gltf) => {
-        const bbox = new THREE.Box3().setFromObject(gltf.scene);
-        const modelSize = bbox.getSize(new THREE.Vector3());
-        gltf.scene.position.y = modelSize.y * -0.5;
-        this.scene.add(gltf.scene);
-      },
-    );
-
-    this.cameraController = new Camera(this);
-
-    this.light();
-    this.effects();
+    this.camera = new THREE.PerspectiveCamera(25, this.getScreenAspectRatio(), 0.1, 1000);
+    this.camera.position.set(4, 1, -4);
+    this.controls = new OrbitControls(this.camera, this.canvas);
   }
 
   light() {
     const ambientLight = new THREE.AmbientLight(0xcccccc, 0.01); // soft white light
     this.scene.add(ambientLight);
-
-    const backLight = new THREE.DirectionalLight(0xffffff, 0.3);
-    backLight.position.set(15, -36, 40);
-    this.scene.add(backLight);
-
-    const keyLight = new THREE.DirectionalLight(0xffffff, 0.5);
-    keyLight.position.set(-36, 36, -40);
-    this.scene.add(keyLight);
   }
 
-  effects() {
-    const renderTarget = new THREE.WebGLRenderTarget(
-      800,
-      600,
-      {
-        samples: this.renderer.getPixelRatio() === 1 ? 2 : 0,
-      },
+  cube() {
+    const test = new THREE.Mesh(
+      new THREE.BoxBufferGeometry(2, 2, 2, 2, 2, 2),
+      new THREE.MeshBasicMaterial(),
     );
+    this.scene.add(test);
 
-    this.composer = new EffectComposer(this.renderer, renderTarget);
-    this.composer.setSize(this.sizes.width, this.sizes.height);
-    this.composer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-    const renderPass = new RenderPass(this.scene, this.camera);
-    this.composer.addPass(renderPass);
-
-    const gammaCorrectionPass = new ShaderPass(GammaCorrectionShader);
-    this.composer.addPass(gammaCorrectionPass);
+    // this.cube = this.gltfLoader.load(
+    //   'natecube.gltf',
+    //   (gltf) => {
+    //     const bbox = new THREE.Box3().setFromObject(gltf.scene);
+    //     const modelSize = bbox.getSize(new THREE.Vector3());
+    //     gltf.scene.position.y = modelSize.y * -0.5;
+    //     this.scene.add(gltf.scene);
+    //   },
+    // );
   }
 
   tick(time) {
@@ -160,10 +119,10 @@ class NateHub {
     this.controls.update();
 
     // Update tween
-    TWEEN.update(time);
+    // TWEEN.update(time);
 
     // Render
-    this.composer.render();
+    this.renderer.render(this.scene, this.camera);
 
     // Call tick again on the next frame
     window.requestAnimationFrame(this.tick.bind(this));
